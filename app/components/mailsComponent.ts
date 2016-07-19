a3899a71f7a22f0dc1127d65b5415ddaef01b16b
htatuan <hoangthoanhtuan@gmail.com>
2016-07-19 22:30:58 +0700

    add create, edit for email

--- app/components/mailsComponent.ts
+++ app/components/mailsComponent.ts
@@ -3,4 +3,20 @@ import {Component} from "angular2/core";
 @Component({
     templateUrl: "app/components/mailsComponents.html"
 })
-export class MailsComponent{}
+export class MailsComponent{
+    public selectedMailItem: any;
+    public mails: Array<any>=
+    [
+            {title: "mail 1", sender:"tuanhoang@gmail.com", date:"1/1/1"},
+             {title: "mail 2", sender:"tuanhaong@yahoo.com", date:"1/1/1"}
+
+    ];
+    public onMailClicked (mailItem :any)
+    {
+         this.selectedMailItem =mailItem;
+        console.log(mailItem.title);
+        
+    }
+    
+   
+}
--- app/layout.ts
+++ app/layout.ts
@@ -2,6 +2,7 @@ import {Component} from "angular2/core";
 import {RouteConfig, RouterOutlet, RouterLink} from "angular2/router";
 import{MailsComponent} from "./components/mailsComponent";
 import{MailDetailComponent} from "./components/mailDetailComponent";
+import{MailCreateComponent} from "./components/mailCreateComponent"
 @Component({
     selector: "default-layout",
     templateUrl: "app/layout.html",
@@ -10,6 +11,7 @@ import{MailDetailComponent} from "./components/mailDetailComponent";
 @RouteConfig([
     {path:"/emails",name:"Mails",component:MailsComponent,useAsDefault:true},
     {path:"/email-detail",name:"MailDetail",component:MailDetailComponent},
+    {path:"/email-create",name:"EmailCreate",component:MailCreateComponent}
 ])
 export class DefaultLayout {
 }
